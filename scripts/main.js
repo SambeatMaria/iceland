53f774be8f5ef790cbc7b4c6ebe1475f44b8060f
SambeatMaria <53336802+SambeatMaria@users.noreply.github.com>
2023-03-06 15:46:26 +0100

    Create navigation structure and related styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log("Reading script");
+const openMobileMenuBtn = document.querySelector(".burger-menu");
+const headerMenu = document.querySelector(".nav-container");
+
+openMobileMenuBtn.addEventListener("click", () => {
+  
+  if(openMobileMenuBtn.classList.contains("open")) {
+    openMobileMenuBtn.classList.remove("open");
+    headerMenu.classList.remove("active");
+  } else {
+    headerMenu.classList.add("active");
+    openMobileMenuBtn.classList.add("open");
+  }
+})
